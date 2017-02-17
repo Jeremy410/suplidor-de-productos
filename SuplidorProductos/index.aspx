dc531e6b02c1e7a818582b04c3edc4dbe7550127
dwane161 <dwane161@gmail.com>
2017-02-17 12:57:20 -0400

    Se agregaron funcionalidades de firebase

--- SuplidorProductos/index.aspx
+++ SuplidorProductos/index.aspx
@@ -4,13 +4,22 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
     <title></title>
 </head>
 <body>
-    <form id="form1" runat="server">
-    <div>
-    
-    </div>
+<form runat="server">
+<div class="form-style-2">
+<div class="form-style-2-heading">Provide your information</div>
+<form action="" method="post">
+<label for="field1"><span>Name <span class="required">*</span></span><asp:TextBox ID="name" runat="server" type="text" class="input-field" name="field1" value=""/></label>
+<label for="field2"><span>Email <span class="required">*</span></span><asp:TextBox ID="email" runat="server" type="text" class="input-field" name="field2" value="" /></label>
+<label><span>Telephone</span><asp:TextBox runat="server" ID="tel1" type="text" class="tel-number-field" name="tel_no_1" value="" maxlength="3" />-<asp:TextBox ID="tel2" runat="server" type="text" class="tel-number-field" name="tel_no_2" value="" maxlength="3"  />-<asp:TextBox ID="tel3" runat="server" type="text" class="tel-number-field" name="tel_no_3" value="" maxlength="4"  /></label>
+<label for="field5"><span>Message <span class="required">*</span></span><asp:TextBox ID="message" runat="server" TextMode="MultiLine" name="field5" class="textarea-field"></asp:TextBox></label>
+
+<label><span>&nbsp;</span><asp:Button runat="server" Text="Submit" OnClick="Unnamed7_Click" /></label>
+</form>
+</div>
     </form>
 </body>
 </html>
--- SuplidorProductos/index.aspx.cs
+++ SuplidorProductos/index.aspx.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +17,54 @@ namespace SuplidorProductos
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack) {
+                requestFireBase();
+            }
+            
+        }
+        public void writeFireBase() { 
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                name = name.Text,
+                email = email.Text,
+                telephones = tel1.Text + "-" + tel2.Text + "-" + tel3.Text,
+                message = message.Text,
+            });
+            var request = WebRequest.CreateHttp("https://suplidorproductos.firebaseio.com/.json");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            var buffer = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            var response = request.GetResponse();
+            json = (new StreamReader(response.GetResponseStream())).ReadToEnd();
+        }
+        public void requestFireBase(){
+            try
+            {
+                var request = WebRequest.CreateHttp("https://suplidorproductos.firebaseio.com/.json");
+                var response = request.GetResponse();
+                var json = (new StreamReader(response.GetResponseStream())).ReadToEnd();
 
+                JObject my_obj = JsonConvert.DeserializeObject<JObject>(json);
+
+                foreach (KeyValuePair<string, JToken> sub_obj in (JObject)my_obj)
+                {
+                    Response.Write("---------------------------------------------------------");
+                    foreach (KeyValuePair<string, JToken> sub_objt in (JObject)my_obj[sub_obj.Key])
+                    {
+                        Response.Write("<h1>" + sub_objt.Key + ": " + "</h1> " + "<h2>" + sub_objt.Value + "</h2>");
+                    }
+                    Response.Write("---------------------------------------------------------");
+                }
+            }
+            catch (Exception ex) { Response.Write(ex.Message); }
+        }
+
+        protected void Unnamed7_Click(object sender, EventArgs e)
+        {
+            writeFireBase();
+            requestFireBase();
         }
     }
 }
